08b221d190535d3e9193a387ae6d62168fb3293e
georgio bejjani <georgiobejjani@georgios-MacBook-Pro.local>
2024-09-10 14:47:39 +0300

    implement new events

--- src/Containers/EventDetails.js
+++ src/Containers/EventDetails.js
@@ -4,21 +4,30 @@ import calendarIcon from '../assets/calendar.png';
 
 export default function EventDetails() {
     const { id } = useParams();
-    // Fetch the event details based on the id (here we use hardcoded data for simplicity)
-    const event = {
+    const event = [{
         id: 1,
-        title: 'Event Title',
-        description: 'Full description of the event goes here. This includes all the details and content that you want to display on this Full description of the event goes here. This includes all the details and content that you want to display on this Full description of the event goes here. This includes all the details and content that you want to display on thisFull description of the event goes here. This includes all the details and content that you want to display on this page. page.page.page.',
-        time: '12 Dec, 12:00 AM'
-    };
-
+        title: 'Mothers day',
+        description: "In light of Mother’s Day and the “Self Love” awareness program, it was a privilege for Fadfed - Release to share a series of awareness sessions to mothers and students at the Secondary Evangelical School in Zahle - Lebanon, accentuating on the importance of self care and love. During session work, it was emphasized that children of all ages should be aware that self love is key to fight and keep going, while taking full support from their mothers who have a vital role in parenting and psychosocial support. Mothers and parents also need to take care of their mental health as well, in order to insure continuity and prosperity. Together, mental health stigma is beatable, one step at a time!",
+        time: '21/3/2024'
+    }, {
+        id: 2,
+        title: 'Awarness session',
+        description: 'An awareness session was held by Fadfed/Release at Body In Motion center - Zahle Bekaa Lebanon concerning the different aspects of bullying at all stages in childhood, teenage years and adulthood.',
+        time: '20/6/2024'
+    },{
+        id: 3,
+        title: 'Chouaa Al Amal',
+        description: 'Zahle hosts a self-care and well being session with their dedicated teachers and monitors',
+        time: '20/7/2024'
+    }];
+    const eventItem = event.find(event => event.id === parseInt(id));
     return (
         <>
-        <div className="event-detail">
-            <h1 className="event-detail-title">{event.title}</h1>
-            <p className="event-detail-description">{event.description}</p>
-            <span className="event-detail-time"><img src={calendarIcon} alt='calendarIcon' className='newsCard__icon'/>{event.time}</span>
-        </div>
+            <div className="event-detail">
+                <h1 className="event-detail-title">{eventItem.title}</h1>
+                <p className="event-detail-description">{eventItem.description}</p>
+                <span className="event-detail-time"><img src={calendarIcon} alt='calendarIcon' className='newsCard__icon' />{eventItem.time}</span>
+            </div>
         </>
     );
 }
